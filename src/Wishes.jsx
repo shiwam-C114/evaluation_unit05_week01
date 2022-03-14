eedf48de5a48e859c844746b272130fd496e201f
shiwam-C114 <93898678+shiwam-C114@users.noreply.github.com>
2022-03-14 21:28:36 +0530

    some improvement in wishlist, adding some functionality

---
+++ src/Wishes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Wishes(props) {
+    return(
+        <div></div>
+    )
+}
+
+export {Wishes}
--- src/wishlist.jsx
+++ src/wishlist.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
+import { Wishes } from './Wishes';
 
 
 function Wishlist() {
 
     let [items, setItem] = useState([])
-    
+
+    // function addItem() {
+    //     if
+    // }
+
+    let flag = true
+    function visbilty() {
+        let txtInp = document.getElementById("item")
+        let btn = document.getElementById("btn")
+        let msg = document.getElementById("msg")
+        if (flag) {
+            txtInp.style.visibility = "visible"
+            btn.style.visibility = "visible"
+            msg.style.visibility = "hidden"
+            flag = false
+        }else {
+            txtInp.style.visibility = "hidden"
+            btn.style.visibility = "hidden"
+            msg.style.visibility = "visible"
+            flag = true
+        }
+    }
+    visbilty()
     function display() {
+
         console.log(items);  
         setItem(document.getElementById("item").value)
         let temp = document.createElement("div")
@@ -19,8 +43,9 @@ function Wishlist() {
      <div>
          <h1>Wishlist</h1>
          <input id='item' type="text" />
-         <button onClick={display}>ADD</button>
-         <div id='disp' ></div>
+         <button id='btn' onClick={display}>ADD</button>
+         <p id='msg'></p>
+         <Wishes items= {items} />
      </div>
  )   
 }
